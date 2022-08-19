2011b60205920ee9b02596a99df2bc56b8eb4e79
SoorajGhosh <57291657+SoorajGhosh@users.noreply.github.com>
2022-08-19 20:58:20 +0530

    Dashboard import error fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Dashboard from './Dashboard/Dashboard';
 import Navbar from './Navbar/Navbar';
-import Dashboard from './Dashboard/Dashboard';
 import "./App.css";
 
 function App() {
